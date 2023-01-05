53cba44b0ef346f7be8e300469e3e06a3270c3c5
Personal <Personal@DESKTOP-L12EV7F>
2023-01-05 11:20:30 +0530

    java sample classes

--- src/test/java/package1/Sample1.java
+++ src/test/java/package1/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1
 	public static void main(String[] args) 
 	{
 		System.out.println("this is the sample first program");
+		System.out.println("There are some modifications in this project");
 	}
 		
 
