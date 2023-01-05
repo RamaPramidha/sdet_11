3c52621f8540d83d872bc14f1ba32a0c8654a378
RKINFOSYS <RKINFOSYS@RAGHAVENDRAREDDY>
2023-01-05 10:47:58 +0530

    this is KRP project Programs

---
+++ java/package2/Sample3.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class Sample3 {
+	public static void main(String[] args) 
+	{
+		System.out.println("this is the third sample program");
+	}
+
+}
---
+++ src/test/java/package1/Sample1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Sample1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("this is the sample first program");
+	}
+		
+
+
+}
